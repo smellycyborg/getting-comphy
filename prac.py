686b2388e1c63227017997d3e142951acaca2c97
chavez <95760172+spumdonut@users.noreply.github.com>
2022-01-01 13:04:32 -0500

    1.3

--- prac.py
+++ prac.py
@@ -1,2 +1,10 @@
-list = [1, 5, 9]
-print(list)
+from array import *
+
+values = array('i', [
+    13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1
+])
+
+newArray = array(values.typecode, (a*a for a in values))
+
+for e in newArray:
+    print(e)
